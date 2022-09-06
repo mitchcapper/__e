59b79ae0533a813d2a6f58c8adc7323722c6be51
Mitch Capper <mitch.capper@gmail.com>
2022-09-06 16:40:08 -0700

    msg

--- hub_build.ps1
+++ hub_build.ps1
@@ -46,7 +46,7 @@ function StatusPrint {
 	Write-Host Freeing up space....
 	ToDelete = @("C:/Program Files/Microsoft Visual Studio", "C:/Program Files (x86)/Android", "C:/Program Files (x86)/Windows Kits", "C:/Program Files (x86)/Microsoft SDKs", "C:/Microsoft/AndroidNDK")
 	ToDelete | foreach { Write-Host Erasing $_; Remove-Item -Recurse -Force $_; }
-	rite-Host Space Feed
+	Write-Host Space Feed
 	StatusPrint
 & "$CefDockerDir\build.ps1" -NoMemoryWarn -Verbose
 
