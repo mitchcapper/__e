3a5b2a353bac6532e342ba905ef1035804b0d513
Mitch Capper <mitch.capper@gmail.com>
2022-09-05 23:34:45 -0700

    msg

--- hub_build.ps1
+++ hub_build.ps1
@@ -8,6 +8,11 @@ try{
 	. (Join-Path $CefDockerDir 'functions.ps1')
 	Set-Location $CefDockerDir
 	Copy (Join-Path $WorkingDir "our_versions.ps1") "versions.ps1"
+	Copy (Join-Path $WorkingDir "our_Dockerfile_vs") "Dockerfile_vs"
+
+	#frees up a good bit of spce on the c drive where docker runs
+	$ToDelete = @("C:/Program Files/Microsoft Visual Studio", "C:/Program Files (x86)/Android", "C:/Program Files (x86)/Windows Kits", "C:/Program Files (x86)/Microsoft SDKs", "C:/Microsoft/AndroidNDK")
+	$ToDelete | foreach { Remove-Item -Recurse -Force $_ }
 
 
 	& "$CefDockerDir\build.ps1" -NoMemoryWarn
